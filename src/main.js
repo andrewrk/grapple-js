2326f90cfcbddc6b2256f026e0d76e9a6933e882
Andrew Kelley <superjoe30@gmail.com>
2014-07-23 23:19:10 -0700

    1080p fullscreen black game that does nothing

---
+++ src/main.js
@@ -0,0 +1,61 @@
+var chem = require("chem");
+var v = chem.vec2d;
+var ani = chem.resources.animations;
+var canvas = document.getElementById("game");
+var engine = new chem.Engine(canvas);
+var Box2D = require('box-2d-web');
+var b2Vec2 = Box2D.Common.Math.b2Vec2;
+var b2BodyDef = Box2D.Dynamics.b2BodyDef;
+var b2Body = Box2D.Dynamics.b2Body;
+var b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+var b2Fixture = Box2D.Dynamics.b2Fixture;
+var b2World = Box2D.Dynamics.b2World;
+var b2MassData = Box2D.Collision.Shapes.b2MassData;
+var b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+var b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+
+engine.buttonCaptureExceptions[chem.button.KeyF5] = true;
+
+engine.showLoadProgressBar();
+engine.start();
+canvas.focus();
+
+chem.resources.on('ready', function () {
+  var batch = new chem.Batch();
+  var boom = new chem.Sound('sfx/boom.ogg');
+  var fpsLabel = engine.createFpsLabel();
+  var gravity = new b2Vec2(0, 9.8);
+  var world = new b2World(gravity, true);
+
+  engine.on('buttondown', function(button) {
+    if (button === chem.button.KeyF11) {
+      fullScreenSomething(canvas);
+    }
+  });
+  engine.on('update', function (dt, dx) {
+  });
+  engine.on('draw', function (context) {
+    // clear canvas to black
+    context.fillStyle = '#000000'
+    context.fillRect(0, 0, engine.size.x, engine.size.y);
+
+    // draw all sprites in batch
+    batch.draw(context);
+
+    // draw a little fps counter in the corner
+    fpsLabel.draw(context);
+  });
+});
+
+function fullScreenSomething(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  }
+}
